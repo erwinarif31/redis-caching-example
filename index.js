89a7d79ac642219ebd2d23230e8e98d36f4a1b03
Erwin Arif <muherwinarif31@gmail.com>
2023-06-18 17:51:35 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import cors from "cors"
+import express from 'express'
+import axios from 'axios'
+import { createClient } from 'redis';
+
+const DEFAULT_EXPIRATION = 3600;
+
+const client = createClient();
+await client.connect()
+
+const app = express()
+app.use(cors())
+
+app.get("/photos", async (req, res) => {
+    const albumId = req.query.albumId
+
+    const value = await client.get(`photos?albumId=${albumId}`)
+
+    if (value) {
+        return res.json(JSON.parse(value))
+    } else {
+        const { data } = await axios.get(
+            "https://jsonplaceholder.typicode.com/photos",
+            { params: { albumId } }
+        )
+        client.setEx(`photos?albumId=${albumId}`, DEFAULT_EXPIRATION, JSON.stringify(data))
+        res.json(data)
+    }
+})
+
+app.listen(3000)
