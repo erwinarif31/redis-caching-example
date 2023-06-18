d28459b85fd24283a96889fafd0d7d340de6a4b0
Erwin Arif <muherwinarif31@gmail.com>
2023-06-18 20:00:02 +0800

    fix: `setEx()` to `set()`

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import cors from "cors"
 import express from 'express'
 import axios from 'axios'
-import { createClient } from 'redis';
-
 const DEFAULT_EXPIRATION = 3600;
+
+import { createClient } from 'redis';
 
 const client = createClient();
 await client.connect()
@@ -14,6 +14,7 @@ app.use(cors())
 app.get("/photos", async (req, res) => {
     const albumId = req.query.albumId
 
+    // mengambil value dari cache
     const value = await client.get(`photos?albumId=${albumId}`)
     
     if (value) {
@@ -26,7 +27,9 @@ app.get("/photos", async (req, res) => {
             "https://jsonplaceholder.typicode.com/photos",
             { params: { albumId } }
         )
-        client.setEx(`photos?albumId=${albumId}`, DEFAULT_EXPIRATION, JSON.stringify(data))
+
+        // menyimpan value ke cache
+        client.set(`photos?albumId=${albumId}`, JSON.stringify(data), "EX", DEFAULT_EXPIRATION)
         res.json(data)
     }
 })
